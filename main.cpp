a903ae0888d7586fe7479b440c56b30fcf564114
frank2 <frank2@dc949.org>
2024-12-15 01:43:53 -0800

    wiping out the CRT is painful

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include <windows.h>
 #include <psapi.h>
 
-int main(int argc, char *argv[]) {
+int main(void *formal) {
    /* enum processes and get pid tied to explorer.exe */
    DWORD proc_array_bytes = 0;
    EnumProcesses(nullptr, 0, &proc_array_bytes);
@@ -14,7 +14,7 @@ int main(int argc, char *argv[]) {
    proc_array_bytes += sizeof(DWORD) * 16;
    
    DWORD *proc_array = (DWORD *)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, proc_array_bytes);
-   assert(EnumProcesses(proc_array, proc_array_bytes, &proc_array_bytes));
+   EnumProcesses(proc_array, proc_array_bytes, &proc_array_bytes);
    /* open pid with PROCESS_QUERY_INFORMATION | PROCESS_VM_READ | PROCESS_CREATE_THREAD | PROCESS_VM_OPERATION | PROCESS_VM_WRITE
     * allocate space for our executable
     * copy the executable in memory and relocate it to the allocated base
